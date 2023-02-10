ad0203e106be6ed42e860b59dd06cfc52c769d1d
richonguzman <richon2001@hotmail.com>
2023-02-10 07:56:22 -0300

    first-beta1

---
+++ src/Lora_APRS_iGate_DIY.cpp
@@ -0,0 +1,143 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <WiFi.h>
+#include "iGate_config.h"
+
+
+#define DBM 20 //20 serian 100mW   # con otro mas potente serian 8=100mA y 20=1000W(1W)
+
+// SPI LoRa Radio
+#define LORA_SCK 5        // GPIO5 - SX1276 SCK
+#define LORA_MISO 19     // GPIO19 - SX1276 MISO
+#define LORA_MOSI 27    // GPIO27 -  SX1276 MOSI
+#define LORA_CS 18     // GPIO18 -   SX1276 CS ---> NSS
+#define LORA_RST 14   // GPIO14 -    SX1276 RST
+#define LORA_IRQ 26  // GPIO26 -     SX1276 IRQ ---->DIO0
+
+
+WiFiClient espClient;
+int status = WL_IDLE_STATUS;
+
+void setup_lora() {
+  Serial.println("Set LoRa pins!");
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ); 
+
+  long freq = 433775000;
+  Serial.print("frequency: ");
+  Serial.println(String(freq));
+  if (!LoRa.begin(freq)) {
+    Serial.println("Starting LoRa failed!");
+    while (true) {
+    }
+  }
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(125000);
+  LoRa.setCodingRate4(5);
+  LoRa.enableCrc();
+  LoRa.setTxPower(20);
+  Serial.println("LoRa init done!");
+}
+
+void setup_wifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("\nConnecting to '"); Serial.print(WIFI_SSID); Serial.println("' WiFi ...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.print("Connected as ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  btStop();
+  setup_lora();
+  
+  Serial.println("");
+  Serial.println("Starting iGate\n");
+}
+
+void procesa_y_sube_APRS_IS(String mensaje) {
+  String packet_para_APRS_IS = "";
+  String callsign_y_path_tracker = "";
+  String gps_info_tracker;
+
+  int posicion_dos_puntos = mensaje.indexOf(':');
+  callsign_y_path_tracker = mensaje.substring(3, posicion_dos_puntos);
+  gps_info_tracker = mensaje.substring(posicion_dos_puntos);
+  packet_para_APRS_IS = callsign_y_path_tracker + ",qAO," + callsign_igate + gps_info_tracker + "\n";
+  //Serial.print("Mensaje APRS_IS    : ");
+  //Serial.println(packet_para_APRS_IS);
+  //packet = "CD2RXU-9>APLT00,qAO,CD2RXU-10:=" + LAT + "/" + LON + ">" +  "\n";
+
+  delay(200);
+  int count = 0;
+  String aprsauth;
+  Serial.println("Conectando a APRS-IS");
+  while (!espClient.connect(SERVER.c_str(), APRSPORT) && count < 20) {
+    Serial.println("Didn't connect with server: " + String(SERVER) + " APRSPORT: " + String(APRSPORT));
+    delay(1000);
+    espClient.stop();
+    espClient.flush();
+    Serial.println("Run client.stop");
+    Serial.println("Trying to connect with server: " + String(SERVER) + " APRSPORT: " + String(APRSPORT));
+    count++;
+    Serial.println("Try: " + String(count));
+  }
+  if (count == 20) {
+    Serial.println("Tried: " + String(count) + " don't send the packet!");
+  } else {
+    Serial.println("Connected with server: " + String(SERVER) + " APRSPORT: " + String(APRSPORT));
+    while (espClient.connected()) {
+      delay(1000);
+      //Serial.println("Run client.connected()");
+      
+      //aprsauth = "user " + USER + " pass " + PAS + "\n";
+      aprsauth = "user " + callsign_igate + " pass " + passcode_igate + "\n"; //info igate
+      espClient.write(aprsauth.c_str());
+      delay(500);
+      //Serial.println("Send client.write=" + aprsauth);
+      
+      espClient.write(packet_para_APRS_IS.c_str());
+      delay(500);
+      //Serial.println("Send espClient.write = " + packet_para_APRS_IS);
+      Serial.println("Packet uploaded =)\n");
+      espClient.stop();
+      espClient.flush();
+      //Serial.println("(Telnet client disconnect)\n");
+    }
+  }
+}
+
+void valida_y_procesa_packet(String mensaje) {
+  String packetStart = "";
+  Serial.print("MENSAJE RECIBIDO!!!   ");
+  Serial.print("(Validando inicio ---> ");
+  packetStart = mensaje.substring(0, 3);
+  if (packetStart == "\x3c\xff\x01") {
+    Serial.println("Packet Valido)");
+    procesa_y_sube_APRS_IS(mensaje);
+  } else {
+    Serial.println("Packet NO Valido)");
+  }
+}
+
+void loop() {  
+  String mensaje_recibido = "";
+  bool valida_inicio;
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    while (LoRa.available()) {
+      int inChar = LoRa.read();
+      mensaje_recibido += (char)inChar;
+    }
+    //Serial.println("Mensaje Recibido   : " + String(mensaje_recibido));
+    valida_y_procesa_packet(mensaje_recibido);
+  }
+}
---
+++ src/iGate_config.h
@@ -0,0 +1,13 @@
+#ifndef IGATE_CONFIG_H_
+#define IGATE_CONFIG_H_
+
+#define WIFI_SSID "Richon"
+#define WIFI_PASSWORD "k4fPnmg5qnyf"
+
+String callsign_igate = "CD2RXU-10";
+String passcode_igate = "23201";
+
+const String SERVER = "brazil.aprs2.net";       // write the address of the aprs server
+const int APRSPORT = 14579;                     // write the aprs server APRSPORT
+
+#endif
