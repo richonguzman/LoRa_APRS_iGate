ff7bf004259f56c651f39de4d80e9eae0a148eb7
richonguzman <richon2001@hotmail.com>
2023-12-24 11:30:19 -0300

    BME680 module test

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -20,7 +20,7 @@
 Configuration   Config;
 WiFiClient      espClient;
 
-String          versionDate           = "2023.12.20";
+String          versionDate           = "2023.12.24";
 int             myWiFiAPIndex         = 0;
 int             myWiFiAPSize          = Config.wifiAPs.size();
 WiFi_AP         *currentWiFi          = &Config.wifiAPs[myWiFiAPIndex];
