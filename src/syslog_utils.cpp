9f4630c8f8d2f126ac7526798deb7bea5f0a1c6e
richonguzman <richon2001@hotmail.com>
2024-01-04 22:12:09 -0300

    syslog update YO3GWM

--- src/syslog_utils.cpp
+++ src/syslog_utils.cpp
@@ -12,18 +12,18 @@ WiFiUDP udpClient;
 namespace SYSLOG_Utils {
 
     void log(String type, String packet, int rssi, float snr, int freqError) {
-        String syslogPacket = "ESP32 LoRa [APRS] - " + Config.callsign + " - ";
+        String syslogPacket = Config.callsign + " LoRa / ";
         if (Config.syslog.active && (stationMode==1 || stationMode==2 || (stationMode==5 && WiFi.status()==WL_CONNECTED))) {
             if (type == "APRSIS Tx") {
                 if (packet.indexOf(":>") > 10) {
-                    syslogPacket += type + " - StartUp STATUS - " + packet.substring(packet.indexOf(":>")+2);
+                    syslogPacket += type + " / StartUp STATUS / " + packet.substring(packet.indexOf(":>")+2);
                 }
             } else if (type == "LoRa Rx") {
                 if (packet.indexOf("::") > 10) {
-                    syslogPacket += type + " - MESSAGE - " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf("::")+2);
+                    syslogPacket += type + " / MESSAGE / " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf("::")+2);
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 } else if (packet.indexOf(":!") > 10 || packet.indexOf(":=") > 10) {
-                    syslogPacket += type + " - GPS - " + packet.substring(3,packet.indexOf(">")) + " / ";
+                    syslogPacket += type + " / GPS / " + packet.substring(3,packet.indexOf(">")) + " / ";
                     if (packet.indexOf("WIDE1-1") > 10) {
                         syslogPacket += packet.substring(packet.indexOf(">")+1,packet.indexOf(",")) + " / WIDE1-1 / ";
                     } else {
@@ -31,31 +31,31 @@ namespace SYSLOG_Utils {
                     }
                     syslogPacket += String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz / " +  GPS_Utils::getDistance(packet);
                 } else if (packet.indexOf(":>") > 10) {
-                    syslogPacket += type + " - STATUS - " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":>")+2);
+                    syslogPacket += type + " / STATUS / " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":>")+2);
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 } else if (packet.indexOf(":`") > 10) {
-                    syslogPacket += type + " - MIC-E - " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":`")+2);
+                    syslogPacket += type + " / MIC-E / " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":`")+2);
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 } else if (packet.indexOf(":T#") >= 10 && packet.indexOf(":=/") == -1) {
-                    syslogPacket += type + " - TELEMETRY - " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":T#")+3);
+                    syslogPacket += type + " / TELEMETRY / " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":T#")+3);
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 } else if (packet.indexOf(":;") > 10) {
-                    syslogPacket += type + " - OBJECT - " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":;")+2);
+                    syslogPacket += type + " / OBJECT / " + packet.substring(3,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf(":;")+2);
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 } else {
-                    syslogPacket += type + " - " + packet;
+                    syslogPacket += type + " / " + packet;
                     syslogPacket += " / " + String(rssi) + "dBm / " + String(snr) + "dB / " + String(freqError) + "Hz";
                 }
             } else if (type == "LoRa Tx") {
                 if (packet.indexOf("RFONLY") > 10) {
-                    syslogPacket += type + " - RFONLY - " + packet.substring(packet.indexOf("::")+2);
+                    syslogPacket += type + " / RFONLY / " + packet.substring(packet.indexOf("::")+2);
                 } else if (packet.indexOf("::") > 10) {
-                    syslogPacket += type + " - MESSAGE - " + packet.substring(0,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf("::")+2);
+                    syslogPacket += type + " / MESSAGE / " + packet.substring(0,packet.indexOf(">")) + " ---> " + packet.substring(packet.indexOf("::")+2);
                 } else {
-                    syslogPacket += type + " - " + packet;
+                    syslogPacket += type + " / " + packet;
                 }
             } else {
-                syslogPacket = "ERROR - Error in Syslog Packet";
+                syslogPacket = "ERROR / Error in Syslog Packet";
             }
             udpClient.beginPacket(Config.syslog.server.c_str(), Config.syslog.port);
             udpClient.write((const uint8_t*)syslogPacket.c_str(), syslogPacket.length());
