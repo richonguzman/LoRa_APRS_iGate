b1bf6fe61f890161e0596213400125fcbcd9054f
SQ2CPA <sq2cpa@gmail.com>
2024-02-24 13:56:08 +0100

    feat: add build workflow & installer

---
+++ installer/install_upgrade.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Your firmware will be upgraded without factory reset.
+echo IF THIS IS YOUR FIRST FLASH ON THIS BOARD PLEASE USE install_with_factory_reset.bat INSTEAD!
+
+set /p port="Enter COM port (for example COM5): "
+
+python.exe bin/esptool/esptool.py --chip esp32 --port "%port%" --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 firmware/bootloader.bin 0x8000 firmware/partitions.bin 0xe000 firmware/boot_app0.bin 0x10000 firmware/firmware.bin
+
+echo "Firmware flashed"
+pause
