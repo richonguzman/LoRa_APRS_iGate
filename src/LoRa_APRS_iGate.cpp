b287704293cf2bab022ef92fa9cf26486ce199ff
richonguzman <richon2001@hotmail.com>
2024-03-18 08:42:18 -0300

    start

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -22,7 +22,7 @@
 Configuration   Config;
 WiFiClient      espClient;
 
-String          versionDate           = "2024.03.10";
+String          versionDate           = "2024.03.18";
 int             myWiFiAPIndex         = 0;
 int             myWiFiAPSize          = Config.wifiAPs.size();
 WiFi_AP         *currentWiFi          = &Config.wifiAPs[myWiFiAPIndex];
