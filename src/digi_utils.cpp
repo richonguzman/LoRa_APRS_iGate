e52ac86e3e337362a2e49b8bf34e1fafffbcd2ca
richonguzman <richon2001@hotmail.com>
2024-03-28 16:28:14 -0300

    sender vs Sender fix

--- src/digi_utils.cpp
+++ src/digi_utils.cpp
@@ -63,8 +63,7 @@ namespace DIGI_Utils {
             if ((packet.substring(0, 3) == "\x3c\xff\x01") && (packet.indexOf("NOGATE") == -1)) {
                 Sender = packet.substring(3, packet.indexOf(">"));
                 if (Sender != Config.callsign) {
-                    String sender = packet.substring(3, packet.indexOf(">"));
-                    STATION_Utils::updateLastHeard(sender);
+                    STATION_Utils::updateLastHeard(Sender);
                     // STATION_Utils::updatePacketBuffer(packet);
                     Utils::typeOfPacket(packet.substring(3), "Digi");
                     AddresseeAndMessage = packet.substring(packet.indexOf("::") + 2);
