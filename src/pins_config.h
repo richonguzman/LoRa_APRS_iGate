0fb2c953386a335c8909e28480f938c3c8554138
richonguzman <richon2001@hotmail.com>
2024-04-15 11:00:35 -0400

    just small typo

--- src/pins_config.h
+++ src/pins_config.h
@@ -10,12 +10,12 @@
 
 // LORA MODULES
 #if defined(TTGO_T_LORA32_V2_1) || defined(HELTEC_V2) || defined(ESP32_DIY_LoRa) || defined(TTGO_T_Beam_V1_2) || defined(TTGO_T_Beam_V1_0)
-#define RADIO_SCLK_PIN    5      // GPIO5    - SX1276 SCK
-#define RADIO_MISO_PIN   19      // GPIO19   - SX1276 MISO
-#define RADIO_MOSI_PIN   27      // GPIO27   - SX1276 MOSI
-#define RADIO_CS_PIN     18      // GPIO18   - SX1276 CS ---> NSS
-#define RADIO_RST_PIN    14      // GPIO14   - SX1276 RST
-#define RADIO_BUSY_PIN   26      // GPIO26   - SX1276 IRQ ---->DIO0
+#define RADIO_SCLK_PIN    5      // GPIO5    - SX1278 SCK
+#define RADIO_MISO_PIN   19      // GPIO19   - SX1278 MISO
+#define RADIO_MOSI_PIN   27      // GPIO27   - SX1278 MOSI
+#define RADIO_CS_PIN     18      // GPIO18   - SX1278 CS ---> NSS
+#define RADIO_RST_PIN    14      // GPIO14   - SX1278 RST
+#define RADIO_BUSY_PIN   26      // GPIO26   - SX1278 IRQ ---->DIO0
 #endif
 
 #if defined(HELTEC_V3) || defined(HELTEC_WS)
