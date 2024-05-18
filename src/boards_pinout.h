166773b81dd0e0c62e082bd6f5b8328caa26e622
richonguzman <richon2001@hotmail.com>
2024-05-18 11:48:50 -0400

    Wemos D1 added

--- src/boards_pinout.h
+++ src/boards_pinout.h
@@ -95,6 +95,19 @@
     #define A7670_RX_PIN    27
 #endif
 
+#ifdef WEMOS_D1_R32_RA02
+    #define RADIO_SCLK_PIN  18
+    #define RADIO_MISO_PIN  19
+    #define RADIO_MOSI_PIN  23
+    #define RADIO_CS_PIN    5
+    #define RADIO_BUSY_PIN  12
+    #define RADIO_RST_PIN   13
+    #define RADIO_DIO1_PIN  14
+    #define OLED_SDA        21
+    #define OLED_SCL        22
+    #define OLED_RST        36
+#endif   
+
 // OLED 
 #if defined(TTGO_T_LORA32_V2_1) || defined(ESP32_DIY_LoRa) || defined(ESP32_DIY_1W_LoRa) || defined(TTGO_T_Beam_V1_0) || defined(TTGO_T_Beam_V1_2) || defined(TTGO_T_Beam_V1_0_SX1268) || defined(TTGO_T_Beam_V1_2_SX1262) || defined(OE5HWN_MeshCom) || defined(ESP32_DIY_LoRa_A7670) || defined(TTGO_T_LORA32_V2_1_915)
     #define OLED_SDA    21
