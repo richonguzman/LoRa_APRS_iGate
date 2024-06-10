ce7ed2519f4489cfe20ddd47665b1c3a1e284e7c
richonguzman <richon2001@hotmail.com>
2024-06-10 13:17:01 -0400

    version update

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -37,7 +37,7 @@ ________________________________________________________________________________
     #include "A7670_utils.h"
 #endif
 
-String          versionDate             = "2024.06.09";
+String          versionDate             = "2024.06.10";
 Configuration   Config;
 WiFiClient      espClient;
 
