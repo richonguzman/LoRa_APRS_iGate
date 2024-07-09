26e2dec2b2d1125996b58b721591462c34ef05a0
richonguzman <richon2001@hotmail.com>
2024-07-09 10:41:26 -0400

    font size update

--- src/display.cpp
+++ src/display.cpp
@@ -11,9 +11,9 @@
         TFT_eSPI tft = TFT_eSPI(); 
 
         #ifdef HELTEC_WIRELESS_TRACKER
-            #define bigSizeFont     2
-            #define smallSizeFont   1
-            #define lineSpacing     9
+            #define bigSizeFont     2.5
+            #define smallSizeFont   1.5
+            #define lineSpacing     12
         #endif
     #else
         #include <Adafruit_GFX.h>
