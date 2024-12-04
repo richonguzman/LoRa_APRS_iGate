008575b4222d040d4f39c598653b476fe433f9fb
richonguzman <richon2001@hotmail.com>
2024-12-04 08:09:54 -0300

    test1

--- variants/XIAO_ESP32S3_WIO_SX1262/board_pinout.h
+++ variants/XIAO_ESP32S3_WIO_SX1262/board_pinout.h
@@ -2,23 +2,19 @@
 #define BOARD_PINOUT_H_
 
     //  LoRa Radio
-    #define HAS_SX1278
-    #define RADIO_SCLK_PIN          15
-    #define RADIO_MISO_PIN          13
-    #define RADIO_MOSI_PIN          12
-    #define RADIO_CS_PIN            14
-    #define RADIO_RST_PIN           2
-    #define RADIO_BUSY_PIN          25
+    #define HAS_SX1262
+    #define RADIO_SCLK_PIN      7
+    #define RADIO_MISO_PIN      8
+    #define RADIO_MOSI_PIN      9
+    #define RADIO_CS_PIN        41
+    #define RADIO_RST_PIN       42
+    #define RADIO_DIO1_PIN      39
+    #define RADIO_BUSY_PIN      40
 
-    //  Display    
-    #define HAS_DISPLAY
+    #define RADIO_HAS_RF_SWITCH //  DIO02
+    #define RADIO_RF_SWITCH     38
 
-    #undef  OLED_SDA
-    #undef  OLED_SCL
-    #undef  OLED_RST
-
-    #define OLED_SDA                5
-    #define OLED_SCL                4
-    #define OLED_RST                -1
-
+    #define BUTTON_PIN          21
+    #define INTERNAL_LED_PIN    48
+    
 #endif
