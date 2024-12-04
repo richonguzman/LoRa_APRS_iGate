27593d87184575d4b0e171c8b7f5c967278b6f4b
richonguzman <richon2001@hotmail.com>
2024-12-04 08:46:07 -0300

    Xiao added

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -48,7 +48,7 @@ ___________________________________________________________________*/
     #include "A7670_utils.h"
 #endif
 
-String              versionDate             = "2024.11.20";
+String              versionDate             = "2024.12.04";
 Configuration       Config;
 WiFiClient          espClient;
 #ifdef HAS_GPS
