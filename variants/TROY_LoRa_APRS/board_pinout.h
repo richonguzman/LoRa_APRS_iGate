59988fbaf1e5242e77236fa43bf7608c1fce24de
richonguzman <richon2001@hotmail.com>
2025-01-05 12:32:46 -0300

    ready for testing

---
+++ variants/TROY_LoRa_APRS/board_pinout.h
@@ -0,0 +1,28 @@
+#ifndef BOARD_PINOUT_H_
+#define BOARD_PINOUT_H_
+
+    //  LoRa Radio
+    #define HAS_SX1278
+    #define RADIO_SCLK_PIN      5
+    #define RADIO_MISO_PIN      19
+    #define RADIO_MOSI_PIN      27
+    #define RADIO_CS_PIN        18
+    #define RADIO_RST_PIN       23
+    #define RADIO_BUSY_PIN      26
+
+    //  Display
+    #define HAS_DISPLAY
+
+    #undef  OLED_SDA
+    #undef  OLED_SCL
+    #undef  OLED_RST
+
+    #define OLED_SDA            21
+    #define OLED_SCL            22
+    #define OLED_RST            -1
+
+    //  OTHER
+    #define INTERNAL_LED_PIN    2
+    #define BATTERY_PIN         35
+
+#endif
