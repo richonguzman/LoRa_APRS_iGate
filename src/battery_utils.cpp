0f2be96227f57a1da9ad36e4dad849c0ec60ec6a
Disappear9 <Disappear9@users.noreply.github.com>
2025-10-02 11:34:13 +0800

    Update battery_utils.cpp

--- src/battery_utils.cpp
+++ src/battery_utils.cpp
@@ -256,43 +256,4 @@ namespace BATTERY_Utils {
         }
     }
 
-    String generateEncodedTelemetryBytes(float value, bool firstBytes, byte voltageType) {  // 0 = internal battery(0-4,2V) , 1 = external battery(0-15V)
-        String encodedBytes;
-        int tempValue;
-
-        if (firstBytes) {
-            tempValue = value;
-        } else {
-            switch (voltageType) {
-                case 0:
-                    tempValue = value * 100;           // Internal voltage calculation
-                    break;
-                case 1:
-                    tempValue = (value * 100) / 2;     // External voltage calculation
-                    break;
-                default:
-                    tempValue = value;
-                    break;
-            }
-        }
-
-        int firstByte   = tempValue / 91;
-        tempValue       -= firstByte * 91;
-
-        encodedBytes    = char(firstByte + 33);
-        encodedBytes    += char(tempValue + 33);
-        return encodedBytes;
-    }
-
-    String generateEncodedTelemetry() {
-        String telemetry = "|";
-        telemetry += generateEncodedTelemetryBytes(telemetryCounter, true, 0);
-        telemetryCounter++;
-        if (telemetryCounter == 1000) telemetryCounter = 0;
-        if (Config.battery.sendInternalVoltage) telemetry += generateEncodedTelemetryBytes(checkInternalVoltage(), false, 0);
-        if (Config.battery.sendExternalVoltage) telemetry += generateEncodedTelemetryBytes(checkExternalVoltage(), false, 1);
-        telemetry += "|";
-        return telemetry;
-    }
-
 }
