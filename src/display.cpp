a1664010a55899c4f7ba2fa26c470f37aae291a0
Craig <cbernstein@cbernstein.com>
2025-11-07 02:02:43 +0000

    display: fix SH1106 begin() condition for Supreme V3 (invert check)

--- src/display.cpp
+++ src/display.cpp
@@ -107,7 +107,7 @@ void displaySetup() {
                 #endif
 
                 #if defined(TTGO_T_Beam_S3_SUPREME_V3)
-                    if (!display.begin(0x3c, false)) {
+                    if (display.begin(0x3c, false)) {
                         displayFound = true;
                         if (Config.display.turn180) display.setRotation(2);
                         display.clearDisplay();
