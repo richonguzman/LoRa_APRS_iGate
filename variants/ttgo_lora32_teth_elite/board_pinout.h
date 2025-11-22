048bd8fc3261fd1fb37cd647341ee01d01b183ab
S57PNX <mitja@muzenic.net>
2025-11-22 14:14:31 +0100

    Initial commit for Lilyo T-ETH Elite. No ethernet or gps yet.

---
+++ variants/ttgo_lora32_teth_elite/board_pinout.h
@@ -0,0 +1,61 @@
+/* Copyright (C) 2025 Ricardo Guzman - CA2RXU
+ * 
+ * This file is part of LoRa APRS iGate.
+ * 
+ * LoRa APRS iGate is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * LoRa APRS iGate is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with LoRa APRS iGate. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef BOARD_PINOUT_H_
+#define BOARD_PINOUT_H_
+
+    //  LoRa Radio
+    #define HAS_SX1262
+    #define RADIO_SCLK_PIN          10
+    #define RADIO_MISO_PIN          9
+    #define RADIO_MOSI_PIN          11
+    #define RADIO_CS_PIN            40
+    #define RADIO_RST_PIN           46
+    #define RADIO_DIO1_PIN          16
+    #define RADIO_BUSY_PIN          16
+    #define RADIO_WAKEUP_PIN        RADIO_DIO1_PIN
+    #define GPIO_WAKEUP_PIN         GPIO_SEL_33
+
+    //  I2C
+    #define USE_WIRE_WITH_OLED_PINS
+
+    //  Display
+    #define HAS_DISPLAY
+
+    #undef  OLED_SDA
+    #undef  OLED_SCL
+    #undef  OLED_RST
+
+    #define OLED_SDA                17
+    #define OLED_SCL                18
+    #define OLED_RST                -1      // Reset pin # (or -1 if sharing Arduino reset pin)
+
+    //  Aditional Config
+    #define INTERNAL_LED_PIN        38      // Green Led
+    #define BATTERY_PIN             1
+
+    // Ethernet
+    #define ETH_MISO_PIN                     47
+    #define ETH_MOSI_PIN                     21
+    #define ETH_SCLK_PIN                     48
+    #define ETH_CS_PIN                       45
+    #define ETH_INT_PIN                      14
+    #define ETH_RST_PIN                      -1
+    #define ETH_ADDR                         1
+
+#endif
