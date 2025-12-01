3416e21a8c7fc71e40a77dabcd0cb27d0f0ff85b
Ricardo Guzman (Richonguzman) <richon2001@hotmail.com>
2025-12-01 10:45:14 -0300

    ready radiolib excludes

--- src/lora_utils.cpp
+++ src/lora_utils.cpp
@@ -93,6 +93,16 @@ namespace LoRa_Utils {
         #if defined(HAS_SX1278) || defined(HAS_SX1276)
             radio.setDio0Action(setFlag, RISING);
         #endif
+
+        /*#ifdef SX126X_DIO3_TCXO_VOLTAGE
+            if (radio.setTCXO(float(SX126X_DIO3_TCXO_VOLTAGE)) == RADIOLIB_ERR_NONE) {
+                Utils::println("Set LoRa Module TCXO Voltage to:" + String(SX126X_DIO3_TCXO_VOLTAGE));
+            } else {
+                Utils::println("Set LoRa Module TCXO Voltage failed! State: " + String(state));
+                while (true);
+        }
+         #endif*/
+         
         radio.setSpreadingFactor(Config.loramodule.rxSpreadingFactor);
         radio.setCodingRate(Config.loramodule.rxCodingRate4);
         float signalBandwidth = Config.loramodule.rxSignalBandwidth/1000;
@@ -102,6 +112,11 @@ namespace LoRa_Utils {
         #if (defined(RADIO_RXEN) && defined(RADIO_TXEN))    // QRP Labs LightGateway has 400M22S (SX1268)
             radio.setRfSwitchPins(RADIO_RXEN, RADIO_TXEN);
         #endif
+
+        /*#ifdef SX126X_DIO2_AS_RF_SWITCH
+        radio.setRfSwitchPins(RADIO_RXEN, RADIOLIB_NC);
+        radio.setDio2AsRfSwitch(true);
+        #endif*/
 
         #ifdef HAS_1W_LORA  // Ebyte E22 400M30S (SX1268) / 900M30S (SX1262) / Ebyte E220 400M30S (LLCC68)
             state = radio.setOutputPower(Config.loramodule.power); // max value 20dB for 1W modules as they have Low Noise Amp
