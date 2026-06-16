cbac8b036523db59e0136b81d3bb36769b5e03a0
Ricardo Guzman (Richonguzman) <richon2001@hotmail.com>
2026-06-16 10:41:44 -0400

    digi patch 1

--- src/LoRa_APRS_iGate.cpp
+++ src/LoRa_APRS_iGate.cpp
@@ -68,7 +68,7 @@ ___________________________________________________________________*/
 #endif
 
 
-String              versionDate             = "2026-06-12";
+String              versionDate             = "2026-06-16";
 String              versionNumber           = "4.0.0";
 Configuration       Config;
 WiFiClient          aprsIsClient;
@@ -196,7 +196,7 @@ void loop() {
                 APRS_IS_Utils::processLoRaPacket(packet); // Send received packet to APRSIS
             }
 
-            if (Config.loramodule.txActive && (Config.digi.mode == 1 || Config.digi.mode == 2 || backupDigiMode)) { // If Digi enabled
+            if (Config.loramodule.txActive && (Config.digi.mode == 1 || Config.digi.mode == 2 || Config.digi.mode == 3 || backupDigiMode)) { // If Digi enabled
                 DIGI_Utils::processLoRaPacket(packet); // Send received packet to Digi
             }
 
